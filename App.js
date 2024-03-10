0c8b24fbd0d030ed8be242e2d488fee7d31a773f
sankalp <160774479+SankalpYerigeri@users.noreply.github.com>
2024-03-11 00:58:25 +0530

    Created different components

--- App.js
+++ App.js
@@ -1,40 +1,96 @@
-import react from "react"
-import  ReactDOM  from "react-dom/client"
+import react from "react";
+import ReactDOM from "react-dom/client";
 
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <div>
+          <img
+            className="logo"
+            src="https://i.ibb.co/zhGxCzf/Pralogo.png"
+            alt="Our Logo"
+          />
+        </div>
 
-const Header = () =>
+        <div className="detail-container">
+          <div className="name">
+            <h1>UVCE Prathibimba</h1>
+            <p>The Art and Photography club of UVCE</p>
+          </div>
+
+          <div className="info">
+            <ul>
+              <li>
+                <i class="fa-solid fa-map-pin"></i>UVCE, Benagaluru
+              </li>
+              <li>
+                <i class="fa-solid fa-cake-candles"></i>19 Nov 2022
+              </li>
+              <li>
+                <i class="fa-regular fa-envelope"></i>uvce.prathibimba@gmail.com
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Cards = () =>
 {
     return (
         <>
-        <div className="header">
-            <div>
-                <img className="logo" src="https://i.ibb.co/zhGxCzf/Pralogo.png" alt="Our Logo"/>
-            </div>
-            <div className="name">
-                <h1>UVCE Prathibimba</h1>
-                
-                <h4>The Art and Photography club of UVCE</h4>
-            </div>
-            <div className="details">
-                <i class="fa-solid fa-location-pin m-0"></i>
-                <h4>UVCE, Benagaluru</h4>
-            </div>
+        <div className="card">
+            <img className="img-holder" src="https://static.vecteezy.com/system/resources/previews/000/649/142/original/team-icon-symbol-sign-vector.jpg"/>
+            <h4 className="card-head">Core Team</h4>
         </div>
         </>
     )
 }
 
-const AppLayout = () =>
-{
-    return (
-        <>
-        <div className="app">
-            <Header/>
+const Body = () => {
+  return (
+    <>
+      <div className="body">
+        <div className="about-us">
+          <h2>About Us</h2>
+          <p>
+            We are a passionate and creative community of artists dedicated to
+            fostering artistic expression and appreciation within our college
+            community. Our club provides a supportive platform for students to
+            explore various forms of art, connect with like-minded individuals,
+            and showcase their talents.
+          </p>
+          <h2>Meet Us</h2>
         </div>
-        </>
-    )
-}
+        
+        <div className="card-container">
+            <Cards />
+            <Cards/>
+            <Cards/>
+            <Cards/>
+            
+            
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+        </div>
+      </div>
+    </>
+  );
+};
 
-root.render(<AppLayout/>)
+const AppLayout = () => {
+  return (
+    <>
+      <div className="app">
+        <Header />
+        <Body />
+      </div>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
