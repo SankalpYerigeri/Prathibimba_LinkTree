924211b17957d5a9e0c5bab85c2c1b0477bc520b
sankalp <160774479+SankalpYerigeri@users.noreply.github.com>
2024-03-10 22:52:45 +0530

    Header Draft

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import react from "react"
 import  ReactDOM  from "react-dom/client"
 
-const HeadingComponent = () =>
+
+const Header = () =>
 {
-    return <h1>UVCE Prathibimba</h1>
+    return (
+        <>
+        <div className="header">
+            <div>
+                <img className="logo" src="https://i.ibb.co/zhGxCzf/Pralogo.png" alt="Our Logo"/>
+            </div>
+            <div className="name">
+                <h1>UVCE Prathibimba</h1>
+                
+                <h4>The Art and Photography club of UVCE</h4>
+            </div>
+            <div className="details">
+                <i class="fa-solid fa-location-pin m-0"></i>
+                <h4>UVCE, Benagaluru</h4>
+            </div>
+        </div>
+        </>
+    )
+}
+
+const AppLayout = () =>
+{
+    return (
+        <>
+        <div className="app">
+            <Header/>
+        </div>
+        </>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<HeadingComponent/>)
+root.render(<AppLayout/>)
