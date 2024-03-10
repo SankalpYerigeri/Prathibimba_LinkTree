d4fa93f668413c9690b28bbd1f7751e97c16f07f
sankalp <160774479+SankalpYerigeri@users.noreply.github.com>
2024-03-10 19:05:31 +0530

    Initial Setup

---
+++ App.js
@@ -0,0 +1,11 @@
+import react from "react"
+import  ReactDOM  from "react-dom/client"
+
+const HeadingComponent = () =>
+{
+    return <h1>UVCE Prathibimba</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<HeadingComponent/>)
